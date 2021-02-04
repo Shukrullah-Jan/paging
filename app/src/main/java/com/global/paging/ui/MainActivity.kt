bc544ec637639bcb9ca807013f125e2817d82c8a
Shukrullah <Almehdipco6766@gmail.com>
2021-02-04 13:30:29 +0500

    first commit

---
+++ app/src/main/java/com/global/paging/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.global.paging.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.global.paging.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
